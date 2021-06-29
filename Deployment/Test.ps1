25c951edd2d4c9206d196efbd17a896e023e4ebc
David Lee <seekdavidlee@gmail.com>
2021-06-29 15:30:44 -0500

    specify dir

--- Deployment/Test.ps1
+++ Deployment/Test.ps1
@@ -1,8 +1,8 @@
-param($TenantId, $StackName, $TestConfig)
+param($TenantId, $StackName, $ReportDirectory, $TestConfig)
 
 $testConfigObj = $TestConfig | ConvertFrom-Json
 $testConfigObj.ApplicationUrl = "https://$StackName.azurewebsites.net"
 $testConfigObj.TenantId = $TenantId
 $testConfigString = $testConfigObj | ConvertTo-Json -Depth 10
 Set-Content -Path Eklee.PasswordManager.Tests/appsettings.json -Value $testConfigString
-dotnet test --logger "junit;LogFilePath=TestResults/results.xml"
+dotnet test --logger "junit;LogFilePath=$ReportDirectory/TestResults/results.xml"
