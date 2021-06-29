dd0de80fd1b9403906f0498839014790b0636105
David Lee <seekdavidlee@gmail.com>
2021-06-29 11:26:23 -0500

    test

--- Deployment/Test.ps1
+++ Deployment/Test.ps1
@@ -1,6 +1,6 @@
 param($TenantId, $StackName, $TestConfig)
 
-$testConfigObj = $TestConfig | ConvertFrom-Json
+$testConfigObj = (Get-Content $TestConfig) | ConvertFrom-Json
 $testConfigObj.ApplicationUrl = "https://$StackName.azurewebsites.net"
 Write-Output ("::debug::Application Url " + $testConfigObj.ApplicationUrl)
 $testConfigObj.TenantId = $TenantId
