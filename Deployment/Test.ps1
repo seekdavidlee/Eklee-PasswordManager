b9ca38337e6b67bae29aa733a9c9a8d2079d0f33
David Lee <seekdavidlee@gmail.com>
2021-06-29 15:55:59 -0500

    output file path

--- Deployment/Test.ps1
+++ Deployment/Test.ps1
@@ -5,4 +5,6 @@ $testConfigObj.ApplicationUrl = "https://$StackName.azurewebsites.net"
 $testConfigObj.TenantId = $TenantId
 $testConfigString = $testConfigObj | ConvertTo-Json -Depth 10
 Set-Content -Path Eklee.PasswordManager.Tests/appsettings.json -Value $testConfigString
-dotnet test --logger "junit;LogFilePath=$ReportDirectory/TestResults/results.xml"
+$testFilePath = "$ReportDirectory/TestResults/results.xml"
+dotnet test --logger "junit;LogFilePath=$testFilePath"
+Write-Output "::set-output name=test_file_path::$testFilePath"
