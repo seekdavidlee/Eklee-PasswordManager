6fc64b06c5a3eafd79f3402ac01570989a27529f
David Lee <seekdavidlee@gmail.com>
2021-06-29 13:22:34 -0500

    debug

--- Deployment/Teardown.ps1
+++ Deployment/Teardown.ps1
@@ -2,9 +2,14 @@ param(
     $StackName,
     $DeployResourceGroupName)
 
-$resources = Get-AzResource -TagName "StackName" -TagValue $StackName -ResourceGroupName $DeployResourceGroupName
+$resources = Get-AzResource -TagName "stack-name" -TagValue $StackName -ResourceGroupName $DeployResourceGroupName
+
+if ($resources.Length -eq 0) {
+    Write-Output "::debug::No resource(s) with tag name 'stack-name' is found."
+}
+
 $resources | ForEach-Object {
     $id = $_.Id 
     Remove-AzResource -ResourceId $id -Force
-    Write-Output "Removed $id"
+    Write-Output "::debug::Removed resource $id"
 }
