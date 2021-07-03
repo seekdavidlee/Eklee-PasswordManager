a93450e6d1a8dde1497fa3c37565bf4c4af388b5
David Lee <seekdavidlee@gmail.com>
2021-07-03 07:09:22 -0500

    fix when test reporting happen

--- Deployment/Test.ps1
+++ Deployment/Test.ps1
@@ -5,6 +5,5 @@ $testConfigObj.ApplicationUrl = "https://$StackName.azurewebsites.net"
 $testConfigObj.TenantId = $TenantId
 $testConfigString = $testConfigObj | ConvertTo-Json -Depth 10
 Set-Content -Path Eklee.PasswordManager.Tests/appsettings.json -Value $testConfigString
-$testFilePath = "$ReportDirectory\TestResults\results.xml"
+$testFilePath = "$ReportDirectory\results.xml"
 dotnet test --logger "junit;LogFilePath=$testFilePath" --filter TestCategory=functional
-Write-Output "::set-output name=test_file_path::$testFilePath"
